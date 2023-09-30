dc1efc049d876e8f2c34b193bc710c5977189a1a
Flornb <bohn.florencia@gmail.com>
2023-09-29 23:45:00 -0300

    footer fixed

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-// menu section
-let visible_menu = false;
-let menu = document.getElementById("nav");
-function showHideMenu(){
-    // console.log("click click")
-    if(visible_menu==false){
-        menu.style.display = "block";
-        visible_menu = true;
-    }
-    else{
-        menu.style.display = "none";
-        visible_menu = false;
-    }
-}
-
-// if select, hide menu 
-let links = document.querySelectorAll ("nav a");
-for(let i = 0; i < links.length; i++){
-    links[i].onClick = function(){
-        menu.style.display = "none";
-        visible_menu = false;
-    }
-}
 
 // nuevo nav opens in small screens
 $(function() {
@@ -35,7 +12,6 @@ $(function() {
   });
 
 
-// footer
 
 
 
